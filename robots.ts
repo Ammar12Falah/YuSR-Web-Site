7c0d7e5ae2301c84b4e623d59329c280f7fdbb81
Ammar12Falah <104951473+Ammar12Falah@users.noreply.github.com>
2026-03-12 17:45:47 +0300

    Delete robots.ts

--- robots.ts
+++
@@ -1,8 +0,0 @@
-import type { MetadataRoute } from "next";
-
-export default function robots(): MetadataRoute.Robots {
-  return {
-    rules: { userAgent: "*", allow: "/" },
-    sitemap: "https://yusr.ai/sitemap.xml", // Update to your actual domain
-  };
-}
